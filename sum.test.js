b0ca365cd1a950c22c49c896072ff74fe81026f0
Cleggatron <david.robert.clegg@gmail.com>
2022-02-27 21:37:26 +0000

    basic tests available within the suite

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,43 @@
 const sum = require("./sum");
 
+//inital test
 test("add 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
 })
+
+//matcher statements
+//to equal recursively checks every field of an array/object to compare exact equality
+test("object assignments", () => {
+    const data = {one: 1};
+    data["two"] = 2;
+
+    expect(data).toEqual({one: 1, two: 2});
+})
+
+//test not equals
+test("adding positive numbers is not zero", () => {
+    for(let a = 1; a < 10; a++){
+        for(let b = 1; b < 10; b++){
+            expect(a + b).not.toBe(0);
+        }
+    }
+})
+
+//test truthy/falsey values
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+  
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
